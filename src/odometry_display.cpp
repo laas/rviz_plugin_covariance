ea6b7f48b0ee5a3c09dd5079cb0aa4a918c916e2
Nils Berg <berg@magazino.eu>
2015-10-14 19:20:25 +0200

    fix odometry display class

--- src/odometry_display.cpp
+++ src/odometry_display.cpp
@@ -4,246 +4,88 @@
 #include <tf/transform_listener.h>
 
 #include <rviz/visualization_manager.h>
-#include <rviz/properties/property.h>
-#include <rviz/properties/property_manager.h>
+#include <rviz/properties/color_property.h>
+#include <rviz/properties/float_property.h>
 #include <rviz/frame_manager.h>
 
 #include "covariance_visual.h"
 
-#include "odometry_display.h"
+#include "odometry_new.h"
 
 namespace rviz_plugin_covariance
 {
-  OdometryDisplay::OdometryDisplay ()
-    : Display (),
-      visual_ (),
-      scene_node_ (),
-      sub_ (),
-      tf_filter_ (),
-      messages_received_ (0),
-      color_ (.8, .2, .8),
-      alpha_ (1.),
-      scale_ (2.),
-      color_property_ (),
-      topic_property_ (),
-      alpha_property_ (),
-      scale_property_ ()
-  {}
+    OdometryDisplay::OdometryDisplay()
+    {
+        color_property_ = new rviz::ColorProperty("Color", QColor( 204, 51, 204 ),
+                                                  "Color to draw the covariance ellipse.",
+                                                  this, SLOT(updateColorAndAlphaAndScale()));
 
-  void OdometryDisplay::onInitialize()
-  {
-    scene_node_ =
-      scene_manager_->getRootSceneNode ()->createChildSceneNode ();
+        alpha_property_ = new rviz::FloatProperty("Alpha", 0.5f,
+                                                  "0 is fully transparent, 1.0 is fully opaque.",
+                                                  this, SLOT(updateColorAndAlphaAndScale()));
 
-    tf_filter_ =
-      new tf::MessageFilter<nav_msgs::Odometry>
-      (*vis_manager_->getTFClient (), "", 100, update_nh_);
-    tf_filter_->connectInput (sub_);
-    tf_filter_->registerCallback
-      (boost::bind(&OdometryDisplay::incomingMessage, this, _1));
+        scale_property_ = new rviz::FloatProperty("Scale", 1.0f,
+                                                  "Scale factor to be applied to covariance ellipse",
+                                                  this, SLOT(updateColorAndAlphaAndScale()));
+    }
 
-    vis_manager_->getFrameManager()->registerFilterForTransformStatusCheck
-      (tf_filter_, this);
-  }
+    void OdometryDisplay::onInitialize()
+    {
+        MFDClass::onInitialize();
+    }
 
-  OdometryDisplay::~OdometryDisplay ()
-  {
-    unsubscribe ();
-    clear ();
-    delete visual_;
-    delete tf_filter_;
-  }
+    OdometryDisplay::~OdometryDisplay()
+    {
+    }
 
-  void OdometryDisplay::clear()
-  {
-    delete visual_;
-    visual_ = 0;
-    tf_filter_->clear ();
-    messages_received_ = 0;
-    setStatus (rviz::status_levels::Warn,
-	       "Topic", "No messages received");
-  }
+    void OdometryDisplay::reset()
+    {
+        MFDClass::reset();
+        //visual_.clear();
+    }
 
-  void OdometryDisplay::setTopic (const std::string& topic)
-  {
-    unsubscribe ();
-    clear ();
-    topic_ = topic;
-    subscribe ();
+    void OdometryDisplay::updateColorAndAlphaAndScale()
+    {
+        Ogre::ColourValue color = color_property_->getOgreColor();
+        float alpha = alpha_property_->getFloat();
+        float scale = scale_property_->getFloat();
 
-    propertyChanged (topic_property_);
+        if (visual_)
+        {
+            visual_->setColor(color.r, color.g, color.b, alpha);
+            visual_->setScale(scale);
+        }
+    }
 
-    causeRender ();
-  }
+    void OdometryDisplay::processMessage(const nav_msgs::Odometry::ConstPtr& msg)
+    {
+        Ogre::Quaternion orientation;
+        Ogre::Vector3 position;
 
-  void OdometryDisplay::setColor (const rviz::Color& color)
-  {
-    color_ = color;
+        if(!context_->getFrameManager()->getTransform(msg->header.frame_id,
+                                                      msg->header.stamp,
+                                                      position, orientation ))
+        {
+            ROS_DEBUG( "Error transforming from frame '%s' to frame '%s'",
+            msg->header.frame_id.c_str(), qPrintable( fixed_frame_ ));
+            return;
+        }
 
-    propertyChanged (color_property_);
-    updateColorAndAlphaAndScale ();
-    causeRender ();
-  }
+        if (!visual_)
+            visual_.reset(new CovarianceVisual(context_->getSceneManager(), scene_node_));
 
-  void OdometryDisplay::setAlpha (float alpha)
-  {
-    alpha_ = alpha;
+        visual_->setMessage (msg->pose);
+        visual_->setFramePosition (position);
+        visual_->setFrameOrientation (orientation);
 
-    propertyChanged (alpha_property_);
-    updateColorAndAlphaAndScale ();
-    causeRender ();
-  }
+        Ogre::ColourValue color = color_property_->getOgreColor();
+        float alpha = alpha_property_->getFloat();
+        float scale = scale_property_->getFloat();
 
-  void OdometryDisplay::setScale (float scale)
-  {
-    scale_ = scale;
-
-    propertyChanged (scale_property_);
-    updateColorAndAlphaAndScale ();
-    causeRender ();
-  }
-
-  void OdometryDisplay::updateColorAndAlphaAndScale ()
-  {
-    if (visual_)
-      {
-	visual_->setColor (color_.r_, color_.g_, color_.b_, alpha_);
-	visual_->setScale (scale_);
-      }
-  }
-
-  void OdometryDisplay::subscribe()
-  {
-    if (!isEnabled())
-      return;
-    try
-      {
-	sub_.subscribe( update_nh_, topic_, 10 );
-	setStatus( rviz::status_levels::Ok, "Topic", "OK" );
-      }
-    catch (ros::Exception& e)
-      {
-	setStatus( rviz::status_levels::Error, "Topic",
-		   std::string( "Error subscribing: " ) + e.what() );
-      }
-  }
-
-  void OdometryDisplay::unsubscribe()
-  {
-    sub_.unsubscribe();
-  }
-
-  void OdometryDisplay::onEnable()
-  {
-    subscribe();
-  }
-
-  void OdometryDisplay::onDisable()
-  {
-    unsubscribe();
-    clear();
-  }
-
-  void OdometryDisplay::fixedFrameChanged()
-  {
-    tf_filter_->setTargetFrame (fixed_frame_);
-    clear();
-  }
-
-  void
-  OdometryDisplay::incomingMessage
-  (const nav_msgs::Odometry::ConstPtr& msg)
-  {
-    ++messages_received_;
-
-    std::stringstream ss;
-    ss << messages_received_ << " messages received";
-    setStatus( rviz::status_levels::Ok, "Topic", ss.str() );
-
-    Ogre::Quaternion orientation;
-    Ogre::Vector3 position;
-    if( !vis_manager_->getFrameManager()->getTransform
-	(msg->header.frame_id,
-	 msg->header.stamp,
-	 position, orientation ))
-      {
-	ROS_DEBUG( "Error transforming from frame '%s' to frame '%s'",
-		   msg->header.frame_id.c_str(), fixed_frame_.c_str() );
-	return;
-      }
-
-    if (!visual_)
-      visual_ = new CovarianceVisual
-	(vis_manager_->getSceneManager (), scene_node_);
-    visual_->setMessage (msg);
-    visual_->setFramePosition (position);
-    visual_->setFrameOrientation (orientation);
-    visual_->setColor (color_.r_, color_.g_, color_.b_, alpha_);
-    visual_->setScale (scale_);
-  }
-
-  void OdometryDisplay::reset ()
-  {
-    Display::reset();
-    clear();
-  }
-
-  void OdometryDisplay::createProperties ()
-  {
-    topic_property_ =
-      property_manager_->createProperty<rviz::ROSTopicStringProperty>
-      ("Topic",
-       property_prefix_,
-       boost::bind (&OdometryDisplay::getTopic, this),
-       boost::bind (&OdometryDisplay::setTopic, this, _1),
-       parent_category_,
-       this );
-    setPropertyHelpText
-      (topic_property_,
-       "nav_msgs::Odometry topic to subscribe to.");
-    rviz::ROSTopicStringPropertyPtr topic_prop = topic_property_.lock ();
-    topic_prop->setMessageType
-      (ros::message_traits::datatype
-       <nav_msgs::Odometry> ());
-
-    color_property_ =
-      property_manager_->createProperty<rviz::ColorProperty>
-      ("Color",
-       property_prefix_,
-       boost::bind (&OdometryDisplay::getColor, this),
-       boost::bind (&OdometryDisplay::setColor, this, _1),
-       parent_category_,
-       this);
-    setPropertyHelpText
-      (color_property_, "Color to draw the ellipse.");
-
-    alpha_property_ =
-      property_manager_->createProperty<rviz::FloatProperty>
-      ("Alpha",
-       property_prefix_,
-       boost::bind (&OdometryDisplay::getAlpha, this),
-       boost::bind (&OdometryDisplay::setAlpha, this, _1),
-       parent_category_,
-       this);
-    setPropertyHelpText
-      (alpha_property_, "0 is fully transparent, 1.0 is fully opaque.");
-
-    scale_property_ =
-      property_manager_->createProperty<rviz::FloatProperty>
-      ("Scale",
-       property_prefix_,
-       boost::bind (&OdometryDisplay::getScale, this),
-       boost::bind (&OdometryDisplay::setScale, this, _1),
-       parent_category_,
-       this);
-    setPropertyHelpText
-      (scale_property_, "Ellipse scale factor.");
-  }
-
+        visual_->setColor(color.r, color.g, color.b, alpha);
+        visual_->setScale(scale);
+    }
 } // end namespace rviz_plugin_covariance
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_DECLARE_CLASS (rviz_plugin_covariance,
-			 OdometryDisplay,
-			 rviz_plugin_covariance::OdometryDisplay,
-			 rviz::Display)
+PLUGINLIB_EXPORT_CLASS(rviz_plugin_covariance::OdometryDisplay, rviz::Display)
--- src/odometry_display.h
+++ src/odometry_display.h
@@ -1,91 +1,47 @@
 // -*- c++-mode -*-
 #ifndef ODOMETRY_DISPLAY_H
-# define ODOMETRY_DISPLAY_H
-# include <message_filters/subscriber.h>
-# include <tf/message_filter.h>
-# include <nav_msgs/Odometry.h>
-# include <rviz/display.h>
+#define ODOMETRY_DISPLAY_H
+
+#include <nav_msgs/Odometry.h>
+#include <rviz/message_filter_display.h>
 
 namespace Ogre
 {
-  class SceneNode;
+    class SceneNode;
+}
+
+namespace rviz
+{
+    class ColorProperty;
+    class FloatProperty;
 }
 
 namespace rviz_plugin_covariance
 {
-  class CovarianceVisual;
+    class CovarianceVisual;
 
-  class OdometryDisplay: public rviz::Display
+    class OdometryDisplay: public rviz::MessageFilterDisplay<nav_msgs::Odometry>
     {
-    public:
-      explicit OdometryDisplay ();
-      virtual ~OdometryDisplay ();
+        Q_OBJECT
+        public:
+            OdometryDisplay();
+            virtual ~OdometryDisplay();
 
-      virtual void onInitialize ();
-      virtual void fixedFrameChanged ();
-      virtual void reset ();
-      virtual void createProperties ();
+        protected:
+            virtual void onInitialize();
+            virtual void reset();
 
-      void setTopic (const std::string& topic);
+        private Q_SLOTS:
+            void updateColorAndAlphaAndScale();
 
-      const std::string& getTopic () const
-      {
-	return topic_;
-      }
+        private:
+            void processMessage(const nav_msgs::Odometry::ConstPtr& msg);
 
-      void setColor (const rviz::Color& color);
+            boost::shared_ptr<CovarianceVisual> visual_;
 
-      const rviz::Color& getColor () const
-      {
-	return color_;
-      }
-
-      void setAlpha (float alpha);
-
-      float getAlpha ()
-      {
-	return alpha_;
-      }
-
-      void setScale (float scale);
-
-      float getScale ()
-      {
-	return scale_;
-      }
-
-    protected:
-      virtual void onEnable ();
-      virtual void onDisable ();
-
-    private:
-      void incomingMessage
-	(const nav_msgs::Odometry::ConstPtr& msg);
-
-      void subscribe ();
-      void unsubscribe ();
-
-      void clear ();
-
-      void updateColorAndAlphaAndScale ();
-
-      CovarianceVisual* visual_;
-
-      Ogre::SceneNode* scene_node_;
-
-      message_filters::Subscriber<nav_msgs::Odometry> sub_;
-      tf::MessageFilter<nav_msgs::Odometry>* tf_filter_;
-      int messages_received_;
-
-      rviz::Color color_;
-      std::string topic_;
-      float alpha_;
-      float scale_;
-
-      rviz::ColorPropertyWPtr color_property_;
-      rviz::ROSTopicStringPropertyWPtr topic_property_;
-      rviz::FloatPropertyWPtr alpha_property_;
-      rviz::FloatPropertyWPtr scale_property_;
+            rviz::ColorProperty* color_property_;
+            rviz::FloatProperty* alpha_property_;
+            rviz::FloatProperty* scale_property_;
     };
 } // end namespace rviz_plugin_covariance
 
