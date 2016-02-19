7a0d141511ee2a001b315e7da8479624a2c4d706
Ellon Mendes <ellon.mendes@laas.fr>
2016-02-19 14:56:32 +0100

    Reset pose with covariance display to rviz's pose display

--- src/pose_with_covariance_display.h
+++ src/pose_with_covariance_display.h
@@ -1,48 +1,86 @@
 // -*- c++-mode -*-
-#ifndef COVARIANCE_DISPLAY_H
-#define COVARIANCE_DISPLAY_H
+#ifndef POSE_WITH_COVARIANCE_DISPLAY_H
+#define POSE_WITH_COVARIANCE_DISPLAY_H
+
+#include <boost/shared_ptr.hpp>
 
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <rviz/message_filter_display.h>
 
-namespace Ogre
-{
-    class SceneNode;
-}
+#include <rviz/message_filter_display.h>
+#include <rviz/selection/forwards.h>
 
 namespace rviz
 {
-    class ColorProperty;
-    class FloatProperty;
+class Arrow;
+class Axes;
+class ColorProperty;
+class EnumProperty;
+class FloatProperty;
+class Shape;
 }
 
 namespace rviz_plugin_covariance
 {
-    class PoseWithCovarianceVisual;
 
-    class PoseWithCovarianceDisplay: public rviz::MessageFilterDisplay<geometry_msgs::PoseWithCovarianceStamped>
-    {
-        Q_OBJECT
-        public:
-            PoseWithCovarianceDisplay();
-            virtual ~PoseWithCovarianceDisplay();
+class CovarianceVisual;
 
-        protected:
-            virtual void onInitialize();
-            virtual void reset();
+class PoseWithCovarianceDisplaySelectionHandler;
+typedef boost::shared_ptr<PoseWithCovarianceDisplaySelectionHandler> PoseWithCovarianceDisplaySelectionHandlerPtr;
 
-        private Q_SLOTS:
-            void updateColorAndAlphaAndScale();
+/** @brief Accumulates and displays the pose from a geometry_msgs::PoseWithCovarianceStamped message. */
+class PoseWithCovarianceDisplay: public rviz::MessageFilterDisplay<geometry_msgs::PoseWithCovarianceStamped>
+{
+Q_OBJECT
+public:
+  enum Shape
+  {
+    Arrow,
+    Axes,
+  };
 
-        private:
-            void processMessage(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
+  PoseWithCovarianceDisplay();
+  virtual ~PoseWithCovarianceDisplay();
 
-            boost::shared_ptr<PoseWithCovarianceVisual> visual_;
+  virtual void onInitialize();
+  virtual void reset();
 
-            rviz::ColorProperty* color_property_;
-            rviz::FloatProperty* alpha_property_;
-            rviz::FloatProperty* scale_property_;
-    };
-} // end namespace rviz_plugin_covariance
+protected:
+  /** @brief Overridden from MessageFilterDisplay to get Arrow/Axes visibility correct. */
+  virtual void onEnable();
 
-#endif // COVARIANCE_DISPLAY_H
+private Q_SLOTS:
+  void updateShapeVisibility();
+  void updateColorAndAlpha();
+  void updateShapeChoice();
+  void updateAxisGeometry();
+  void updateArrowGeometry();
+
+private:
+  void clear();
+
+  virtual void processMessage( const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& message );
+
+  rviz::Arrow* arrow_;
+  rviz::Axes* axes_;
+  bool pose_valid_;
+  PoseWithCovarianceDisplaySelectionHandlerPtr coll_handler_;
+
+  rviz::EnumProperty* shape_property_;
+
+  rviz::ColorProperty* color_property_;
+  rviz::FloatProperty* alpha_property_;
+
+  rviz::FloatProperty* head_radius_property_;
+  rviz::FloatProperty* head_length_property_;
+  rviz::FloatProperty* shaft_radius_property_;
+  rviz::FloatProperty* shaft_length_property_;
+
+  rviz::FloatProperty* axes_length_property_;
+  rviz::FloatProperty* axes_radius_property_;
+
+  friend class PoseWithCovarianceDisplaySelectionHandler;
+};
+
+} // namespace rviz_plugin_covariance
+
+#endif // POSE_WITH_COVARIANCE_DISPLAY_H
