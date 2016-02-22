ca71f8a436033f7acd2e4f377d658eaebfe45ab7
Ellon Mendes <ellon.mendes@laas.fr>
2016-02-22 21:49:40 +0100

    Add orientation covariance to odom test and also send transform to child_frame_id through TF

--- scripts/send_odometry.py
+++ scripts/send_odometry.py
@@ -3,6 +3,9 @@
 import roslib; roslib.load_manifest('rviz')
 from nav_msgs.msg import Odometry
 import rospy
+import tf
+
+br = tf.TransformBroadcaster()
 
 topic = 'test_odometry'
 publisher = rospy.Publisher(topic, Odometry)
@@ -14,6 +17,7 @@ while not rospy.is_shutdown():
 
    odo = Odometry()
    odo.header.frame_id = "/base_link"
+   odo.child_frame_id = "/odom_msg"
    odo.header.stamp = rospy.Time.now()
    
    odo.pose.pose.position.x = 0
@@ -30,10 +34,15 @@ while not rospy.is_shutdown():
    odo.pose.covariance[0+0*6] = 0.2+(abs(y)/2.5);
    odo.pose.covariance[1+1*6] = 0.2;
    odo.pose.covariance[2+2*6] = 0.2;
-   odo.pose.covariance[3+3*6] = -1;
-   odo.pose.covariance[4+4*6] = -1;
-   odo.pose.covariance[5+5*6] = -1;
+   odo.pose.covariance[3+3*6] = 0.1;
+   odo.pose.covariance[4+4*6] = 0.1;
+   odo.pose.covariance[5+5*6] = 0.1;
 
+   br.sendTransform((odo.pose.pose.position.x, odo.pose.pose.position.y, odo.pose.pose.position.z),
+      (odo.pose.pose.orientation.x,odo.pose.pose.orientation.y,odo.pose.pose.orientation.z,odo.pose.pose.orientation.w),
+      odo.header.stamp,
+      "odom_msg",
+      "base_link")
 
    publisher.publish( odo )
 
